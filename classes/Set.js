b79347b0b0c2932f90ad7797dce939f9e762b9b7
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-11-12 21:27:14 -0500

    Added Set definition and finished test script.

--- classes/Set.js
+++ classes/Set.js
@@ -4,20 +4,80 @@ class Set {
     this._storage = [];
   }
 
+  // Check for the provided value in the set.
   has (value) {
-
+    return this._storage.indexOf(value) !== -1;
   }
 
+  // Adds the provided value to the set. Returns boolean for success.
   add (value) {
-
+    if (!this.has(value)) {
+      this._storage.push(value);
+      return true;
+    }
+    return false;
   }
 
+  // Removes the provided value if it's in the set. Returns boolean for success.
   remove (value) {
-
+    if (this.has(value)) {
+      this._storage.splice(this._storage.indexOf(value), 1);
+      return true;
+    }
+    return false;
   }
 
+  // Returns all the values in the set.
   values () {
+    return this._storage;
+  }
+
+  // Returns length of the set.
+  length () {
+    return this._storage.length;
+  }
+
+  // Returns the union of two sets.
+  union (set) {
+    let union = new Set();
+    let first = this.values();
+    let second = set.values();
+
+    first.forEach(v => { union.add(v); });
+    second.forEach(v => { union.add(v); });
+    return union;
+  }
+
+  // Returns the intersection of two sets.
+  intersection (set) {
+    let intersection = new Set();
     
+    this._storage.forEach(v => { 
+      if (set.has(v))
+        intersection.add(v);
+    });
+    return intersection;
+  }
+
+  // Returns the difference of two sets.
+  difference (set) {
+    let difference = new Set();
+
+    this._storage.forEach(v => {
+      if (!set.has(v))
+        difference.add(v);
+    });
+    return difference;
+  }
+
+  // Returns a boolean representing if this set is a subset of another set.
+  isSubset (set) {
+    return this._storage.every(v => set.has(v));
+  }
+
+  // Checks if the set is empty.
+  isEmpty () {
+    return this._storage.length === 0;
   }
 }
 
--- set.js
+++ set.js
@@ -1,3 +1,35 @@
 const Set = require('./classes/Set');
 
-let s = new Set();
+let s1 = new Set();
+
+s1.add(1);
+s1.add(2);
+s1.add(3);
+console.log(`Set contents: ${s1.values()}.`);
+
+s1.add(2);
+console.log(`Set contents: ${s1.values()}.`);
+
+s1.remove(2);
+console.log(`Set contents: ${s1.values()}.`);
+
+console.log(`Set has 2: ${s1.has(2)}.`);
+
+let s2 = new Set();
+s2.add(2);
+s2.add(4);
+s2.add(3);
+s2.add(6);
+s2.add(8);
+
+let s3 = new Set();
+s3.add(2);
+s3.add(4);
+
+console.log(`Set 1: ${s1.values()}.`);
+console.log(`Set 2: ${s2.values()}.`);
+console.log(`Set 3: ${s3.values()}.`);
+console.log(`Union s1, s2: ${s1.union(s2).values()}.`);
+console.log(`Intersection s1, s2: ${s1.intersection(s2).values()}.`);
+console.log(`Difference s1, s2: ${s1.difference(s2).values()}.`);
+console.log(`Subset s2, s3: ${s2.isSubset(s3)}.`);

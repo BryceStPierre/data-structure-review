f3f7bb244c8ba44183271069a84b24102e2138af
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-11-12 21:29:26 -0500

    Added comments to Stack structure.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Common data structures implemented in JavaScript.
 
 Data structures implemented:
 1. Stack
+2. Set
--- classes/Stack.js
+++ classes/Stack.js
@@ -1,28 +1,30 @@
 class Stack {
 
-  // Assumption:
-  //
-  // No space limitation, exploiting JavaScript's Array.
   constructor () {
     this._storage = [];
   }
 
+  // Pushes a new value onto the stack.
   push (value) {
     this._storage.push(value);
   }
 
+  // Pops a value from the stack.
   pop () {
     return this._storage.pop();
   }
 
+  // Returns value last pushed to the stack.
   peek () {
     return this._storage[this._storage.length - 1];
   }
 
+  // Returns the length of the stack.
   length () {
     return this._storage.length;
   }
 
+  // Checks if the stack is empty.
   isEmpty () {
     return this._storage.length === 0;
   }
